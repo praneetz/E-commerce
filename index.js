f5924d491724f5015891f6cbe6ab950cb272404f
Amit Thakur <amitthakur.softprodigy@gmail.com>
2022-06-21 14:39:51 +0530

    style added!

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const ejs=require("ejs")
+const path=require("path")
 
-require("./database");
+require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 3090;
 
-app.get("/", (req, res) => res.send("E-Commerce Website!"));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs")
+
+app.get("/", (req, res) => res.render("Home"));
+app.get("/register",(req,res)=>{
+    res.render("Register")
+})
 
 app.listen(port, () =>
   console.log(`app is running on http://localhost:${port}`)
